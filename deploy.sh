16f0fc66d64166a8681b9bd5bb91c29e7ad53e59
Michael Ymbong <5406654+michaelymbong@users.noreply.github.com>
2022-02-21 14:10:45 +0800

    ci(circleci): setup

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Current user
+who
+
+# Load PATH, profiles, and other bash settings
+echo "Loading bash profile and settings"
+export PATH=~/.nvm/versions/node/v16.14.0/bin:$PATH
+source ~/.bashrc
+source ~/.profile
+echo "PATH:"
+echo "$PATH"
+
+# Navigate to temp dir and pull changes
+cd ~/services/flow-fields-try
+git pull origin main
+
+# Install node_modules
+echo "Node version"
+node -v
+rm -rf node_modules
+npm install
+
+## Build project
+echo "Building..."
+npm run  build
+
+# Copy build files to 
+rm -rf /var/www/html/flowfields.michaelymbong.art
+cp -r /build /var/www/html/flowfields.michaelymbong.art
+
+echo "DONE!"
